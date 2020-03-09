4bdff377cbcb07781adfc7f324f66ca49ca0490a
MarcosRecio <33786384+MarcosRecio@users.noreply.github.com>
2020-03-09 16:13:56 +0100

    Update Powerset.py

--- Powerset.py
+++ Powerset.py
@@ -9,4 +9,8 @@ lengths = lambda x: [len(i) for i in powerlist(x)]
 #Number of subsets ordered by cardinality
 powernums = lambda x: sorted(list(set([(t, lengths(x).count(t)) for t in lengths(x)])), key = lambda y: y[0])
 
-print(powernums(list(range(6))))
+# Example
+print(powernums(list(range(6)))) 
+
+# Output
+[(0, 1), (1, 6), (2, 15), (3, 20), (4, 15), (5, 6), (6, 1)]
