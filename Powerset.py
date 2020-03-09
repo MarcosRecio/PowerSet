8afb16f1ccfe62330d54f108b1a086437f8da1ab
MarcosRecio <33786384+MarcosRecio@users.noreply.github.com>
2020-03-09 16:53:00 +0100

    Update Powerset.py

--- Powerset.py
+++ Powerset.py
@@ -1,6 +1,6 @@
 #Powerset of a List
 
-powerlist = lambda s: sorted([[s[j] for j in range(len(s)) if (i & (1 << j))] for i in range(1 << len(s))]
+powerset = lambda s: sorted([[s[j] for j in range(len(s)) if (i & (1 << j))] for i in range(1 << len(s))]
                             , key = lambda y: len(y))
 
 # Lengths of the sublists (subsets), cardinality of the subsets
@@ -10,7 +10,7 @@ lengths = lambda x: [len(i) for i in powerlist(x)]
 powernums = lambda x: sorted(list(set([(t, lengths(x).count(t)) for t in lengths(x)])), key = lambda y: y[0])
 
 # Example
-print(powernums(list(range(6)))) 
+#print(powernums(list(range(6)))) 
 
 # Output
-[(0, 1), (1, 6), (2, 15), (3, 20), (4, 15), (5, 6), (6, 1)]
+#[(0, 1), (1, 6), (2, 15), (3, 20), (4, 15), (5, 6), (6, 1)]
